baad9bcb83b9ddd5bc353237aa74929f12ac82ef
vikash <kumarvikash2740@gmail.com>
2019-02-07 11:37:14 +0530

    code optimized

---
+++ carousel.php
@@ -0,0 +1,43 @@
+<!-- Indicators -->
+<ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="ny.jpg" alt="New York" width="1200" height="700">
+        <div class="carousel-caption">
+          <h3>Temple1</h3>
+          <p>Devote1</p>
+        </div>      
+      </div>
+
+      <div class="item">
+        <img src="chicago.jpg" alt="Chicago" width="1200" height="700">
+        <div class="carousel-caption">
+			<h3>Temple2</h3>
+			<p>Devote2</p>
+        </div>      
+      </div>
+    
+      <div class="item">
+        <img src="la.jpg" alt="Los Angeles" width="1200" height="700">
+        <div class="carousel-caption">
+			<h3>Temple3</h3>
+			<p>Devote3</p>
+        </div>      
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
---
+++ tabs.php
@@ -0,0 +1,97 @@
+<ul class="nav nav-tabs">
+		  <li class="active"><a data-toggle="tab" href="#home"><b>Near by places</b></a></li>
+		  <li><a data-toggle="tab" href="#menu1"><b>How to reach?</b></a></li>
+		  <li><a data-toggle="tab" href="#menu2"><b>Main Attraction</b></a></li>
+		  
+		</ul>
+	  
+		<div class="tab-content">
+		  <div id="home" class="tab-pane fade in active">
+			
+				<div class="row row-margin-top">
+					<div class="col-xs-12 col-sm-4 col-lg-4 col-md-12">
+						<div class="panel panel-danger">
+							<div class="panel-heading">
+								<img class ="image-center" src="goddess.png" alt="Goddess" style="width:25%;">
+								<p class="text-center">
+									<b>Surya Mandir,Bundu</b>
+								</p>
+							</div>
+							<div class="panel-body">
+									About 39 km. from Ranchi on Tata Road near Bundu, stands the elegant Sun Temple fashioned in the form of huge chariot with richly decorated 18 wheels (nine on each side) and seven life like horses ready to take off.
+							</div>
+						  </div>
+						  </div>
+						  <div class="col-xs-12 col-sm-4 col-lg-4 col-md-12">
+								<div class="panel panel-danger">
+									<div class="panel-heading">
+										<img class ="image-center" src="goddess.png" alt="Goddess" style="width:25%;">
+										<p class="text-center">
+											<b>Surya Mandir,Bundu</b>
+										</p>
+									</div>
+									<div class="panel-body">
+											About 39 km. from Ranchi on Tata Road near Bundu, stands the elegant Sun Temple fashioned in the form of huge chariot with richly decorated 18 wheels (nine on each side) and seven life like horses ready to take off.
+									</div>
+								  </div>
+								  </div>
+								  <div class="col-xs-12 col-sm-4 col-lg-4 col-md-12">
+										<div class="panel panel-danger">
+											<div class="panel-heading">
+												<img class ="image-center" src="goddess.png" alt="Goddess" style="width:25%;">
+												<p class="text-center">
+													<b>Surya Mandir,Bundu</b>
+												</p>
+											</div>
+											<div class="panel-body">
+													About 39 km. from Ranchi on Tata Road near Bundu, stands the elegant Sun Temple fashioned in the form of huge chariot with richly decorated 18 wheels (nine on each side) and seven life like horses ready to take off.
+											</div>
+										  </div>
+										  </div>
+					</div>
+					<div class="row" style="float:right;">
+							<button type="button" class="btn btn-danger" style="background-color: #675959;    border-color: #675859; ">More</button>
+					</div>
+		  </div>
+		  <div id="menu1" class="tab-pane fade">
+				<div class="row">
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<div class ="image-center" id="googleMap" style="width:100%;height: 210px;"></div>
+								<p><em>Dewri Mandir is located in Tamar 60 km toward south from the capital of Jharkhand i.e. Ranchi. It is in the Tata-Ranchi  Highway (NH33). It is a very old temple of Goddess Durga. </em></p>
+								<p>You can reach here by ROAD(NH3) either from Ranchi or Jamshedpur.</p>
+								<p>Public Bus can be found from either Ranchi or Jamshedpur. </p>
+								<p>You can also hire Private vehicles to visit.     </p>
+								<ul class="maadewri_list">
+									<li>Nearby Airport:-Ranchi,Jamshedpur(Tatanagar) </li>
+									<li>Nearby Railway Station:-Tatanagar,Ranchi,Muri</li>
+									<li>Nearby Cities:-Ranchi,Jamshedpur </li>	
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="row" style="float:right;">
+							<button type="button" class="btn btn-danger" style="background-color: #675959;    border-color: #675859; ">More</button>
+					</div>
+		  </div>
+		  <div id="menu2" class="tab-pane fade">
+				<div class="row">
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<img class ="image-center" src="temple-280x140.jpg" alt="Goddess" style="width:100%;">
+								<p><em>The main attraction of MAA DEWRI TEMPLE ,Which distinguised it are:-</em></p>
+								<ul class="maadewri_list">
+									<li>the idol is having 16 Hands (Normally Goddess Durga is having 8 Hands).</li>
+									<li>Dedicated to Solha Bhuji Goddess, an avatar of Goddess Durga.</li>
+									<li>Dewri Temple is also believed to be the only temple where 6 tribal priests, known as Pahans, perform rituals and offer prayers alongside the Brahmin priests.</li>	
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="row" style="float:right;">
+							<button type="button" class="btn btn-danger" style="background-color: #675959;    border-color: #675859; ">More</button>
+					</div>
+		  </div>
+		  
+        </div>
+        
