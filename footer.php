858c56ed0cc1114ffc6a8207eadd1e18f42d475d
vikash <kumarvikash2740@gmail.com>
2019-02-07 10:33:22 +0530

    homepage display

---
+++ footer.php
@@ -0,0 +1,60 @@
+<!-- Footer -->
+<footer>
+	<p class="text-center">
+  <a class="up-arrow text-center" href="#myPage" data-toggle="tooltip" title="TO TOP">
+    <span class="glyphicon glyphicon-chevron-up "></span>
+  </a></p><br><br>
+  <div class="row">
+		
+		<div class="col-xs-12 col-sm-3 col-lg-3 col-md-12 heading">
+			<h5>Important Links</h5>
+			<ul class="footer_link">
+				<li> <a href="http://www.jharkhandtourism.in" target="_blank">Jharkhand Tourism</a></li>
+				<li> <a href="http://ranchi.nic.in" target="_blank">Ranchi District</a></li>
+				<li> <a href="http://www.jharkhand.gov.in" target="_blank">Govt.Of Jharkhand</a></li>
+				<li> <a href="http://india.gov.in" target="_blank">Govt.Of India</a></li>
+				<li> <a href="http://www.incredibleindia.org" target="_blank">Incredible India</a></li>
+				<li> <a href="http://www.tourism.gov.in" target="_blank">Ministry of Tourism</a></li>
+			</ul>
+		</div>
+		
+		<div class="col-xs-12 col-sm-3 col-lg-3 col-md-12 heading">
+			<h5>Important Temples In India</h5>
+			<ul class="footer_link">
+				<li> <a href="http://www.jagannath.nic.in" target="_blank">Jagannath Temple,Puri</a></li>
+				<li> <a href="http://www.siddhivinayak.org" target="_blank">Shree Siddhivinayak Ganapati Temple</a></li>
+				<li> <a href="http://www.shirdisaitemple.com" target="_blank">Shirdi Saibaba Temple</a></li>
+				
+				<li> <a href="http://www.mahakaleshwar.nic.in" target="_blank">Mahakaleshwar Temple</a></li>
+				<li> <a href="http://www.somnath.org" target="_blank">Jay Somnath</a></li>
+				 <li> <a href="www.balajitemple.com" target="_blank">Balaji Temple Of Tirupati</a></li>
+			</ul>
+		</div>
+		
+		<div class="col-xs-12 col-sm-3 col-lg-3 col-md-12 heading">
+			<h5>Important Durga Mata Temples</h5>
+			<ul class="footer_link">
+				<li><a href="http://maavaishnodevi.org">SHRI MATA VAISHNO DEVI</a></li>
+				<li><a href="http://www.shreeshantadurga.com">Shree Shantadurga Temple</a></li>
+				<li><a href="http://www.kateeldevi.in">Kateel</a></li>
+				<li><a href="http://www.durgamma.com">Kanaka Durga temple</a></li>
+			
+			</ul>
+		</div>
+		
+		<div class="col-xs-12 col-sm-3 col-lg-3 col-md-12 heading">
+			<h5>Social Links</h5>
+			<ul class="footer_link">
+				<li><a href="#" class="facebook social">Facebook</a></li>
+				<li><a href="#" class="linkedin social">Linkedin</a></li>
+				<li><a href="#" class="myspace social">Myspace</a></li>
+				<li><a href="#" class="youtube social">Youtube</a></li>
+				<li><a href="#" class="vimeo social">Vimeo</a></li>
+			</ul>
+		</div>
+		<br>
+
+	
+	</div>
+  <p class="text-center">Copyright © 2019 <a href="#" data-toggle="tooltip" title="Temple">Temple_name</a></p> 
+</footer>
---
+++ header.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage">MANDIR</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+		<li><a href="#myPage">HOME</a></li>
+		<li class="dropdown">
+			<a class="dropdown-toggle" data-toggle="dropdown" href="#">THE MANDIR
+			<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+			  <li><a href="maa_durga.php">MAA DURGA</a></li>
+			  <li><a href="#">ABOUT DEWRI</a></li>
+			</ul>
+		  </li>
+		  <li class="dropdown">
+			<a class="dropdown-toggle" data-toggle="dropdown" href="#">Pilgrim Facilities
+			<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+			  <li><a href="#">Guest Houses</a></li>
+			  <li><a href="#">Trasportation</a></li>
+			</ul>
+		  </li>
+        <li><a href="#band">Photo Gallery</a></li>
+        <li><a href="#tour">Donate</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
